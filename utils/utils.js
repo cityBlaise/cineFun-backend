0083c86e95a7993c2d934abc3242e99484ccdb1b
cityBlaise <cityjmo@gmail.com>
2023-02-10 19:41:57 +0100

    fixed the redirection when sending bad params

--- utils/utils.js
+++ utils/utils.js
@@ -12,5 +12,5 @@ dotenv.config()
 const port = process.env.PORT || 1998
 const databasehost = process.env.Mongo_dev || process.env.Mongo_prod;
 const protocol = (process.env?.NODE_ENV && process.env?.NODE_ENV ==='prod')? https: http;
-const host = process.env.Prod_Host?`${process.env.Prod_Host}${port}`: `${process.env.Dev_Host}${port}`;
+const host = process.env.Prod_Host?`${process.env.Prod_Host}`: `${process.env.Dev_Host}${port}`;
 export  {protocol, host, port,error, succes, databasehost};
