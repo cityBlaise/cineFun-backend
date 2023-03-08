f63b95edfb8a25428d8d5edcb4a536cc0d01bedb
cityBlaise <cityjmo@gmail.com>
2023-03-08 08:35:24 +0100

    remove unusefull files and folders

--- app.js
+++ app.js
@@ -66,6 +66,7 @@ app.use((req, res, next) => {
  */
 setRoutes(app); 
 
+
 // eslint-disable-next-line no-unused-vars
 app.use((err, req, res, next) => {
   erreur("Server Internal error\n"+err.message+"\n"+ err.stack);
